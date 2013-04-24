9d3f085a0a42f943cbfa56a7e7f9ebe897bdf9ad
Simon Jefford <simon.jefford@gmail.com>
2013-04-24 09:15:46 +0100

    Ido everywhere please

--- personal/_personalinit.el
+++ personal/_personalinit.el
@@ -1,4 +1,4 @@
-(prelude-ensure-module-deps '(smex))
+(prelude-ensure-module-deps '(smex ido-ubiquitous))
 
 (defmacro bind (key fn)
   "shortcut for global-set-key"
@@ -16,3 +16,14 @@
 (set-face-font 'default "-apple-Menlo-medium-normal-normal-*-11-*-*-*-m-0-iso10646-1")
 (dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
   (when (fboundp mode) (funcall mode -1)))
+
+(ido-mode t)
+(ido-ubiquitous t)
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-auto-merge-work-directories-length nil
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point 'guess
+      ido-use-virtual-buffers t
+      ido-handle-duplicate-virtual-buffers 2
+      ido-max-prospects 10)
