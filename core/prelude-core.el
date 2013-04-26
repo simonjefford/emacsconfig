6dc30434afe20d64317b009e10c7bb0468a8e1b3
Simon Jefford <simon.jefford@gmail.com>
2013-04-26 11:20:24 +0100

    Do not want this sudo stuff

--- core/prelude-core.el
+++ core/prelude-core.el
@@ -262,12 +262,6 @@ buffer is not visiting a file."
                          (ido-read-file-name "Find file(as root): ")))
     (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
 
-(defadvice ido-find-file (after find-file-sudo activate)
-  "Find file as root if necessary."
-  (unless (and buffer-file-name
-               (file-writable-p buffer-file-name))
-    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
-
 (defun prelude-switch-or-start (function buffer)
   "If BUFFER is current, bury it, otherwise invoke FUNCTION."
   (if (equal (buffer-name (current-buffer)) buffer)
