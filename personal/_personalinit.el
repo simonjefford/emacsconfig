3eb0ed086bc8354dc4da796100f51cec6828b30b
Simon Jefford <simon.jefford@gmail.com>
2013-04-29 16:22:31 +0100

    Turns out this binding is crucial...

--- personal/_personalinit.el
+++ personal/_personalinit.el
@@ -5,6 +5,12 @@
                    ,(if (listp fn)
                         fn)))
 
+(if (fboundp 'smex-initialize)
+    (progn
+      (bind "C-x C-m" 'smex)
+      (bind "M-x" 'smex-major-mode-commands))
+  (message "install smex!"))
+
 (set-face-font 'default "-apple-Menlo-medium-normal-normal-*-11-*-*-*-m-0-iso10646-1")
 (dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
   (when (fboundp mode) (funcall mode -1)))
