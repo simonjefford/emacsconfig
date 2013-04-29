1236d077a0f006aa4ec3b1cb6619ce1ed7ad138e
Simon Jefford <simon.jefford@gmail.com>
2013-04-29 16:07:59 +0100

    smex + ido-ubiquitous handled by upstream now

--- personal/_personalinit.el
+++ personal/_personalinit.el
@@ -1,5 +1,3 @@
-(prelude-ensure-module-deps '(smex ido-ubiquitous))
-
 (defmacro bind (key fn)
   "shortcut for global-set-key"
   `(global-set-key (kbd ,key)
@@ -7,18 +5,10 @@
                    ,(if (listp fn)
                         fn)))
 
-(if (fboundp 'smex-initialize) ;; already initialised by starter-kit
-    (progn
-      (bind "C-x C-m" 'smex)
-      (bind "M-x" 'smex-major-mode-commands))
-  (message "install smex!"))
-
 (set-face-font 'default "-apple-Menlo-medium-normal-normal-*-11-*-*-*-m-0-iso10646-1")
 (dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
   (when (fboundp mode) (funcall mode -1)))
 
-(ido-mode t)
-(ido-ubiquitous t)
 (setq ido-enable-prefix nil
       ido-enable-flex-matching t
       ido-auto-merge-work-directories-length nil
