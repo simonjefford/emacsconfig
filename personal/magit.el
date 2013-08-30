d14f0baca354f7830e8009268e8a72308aee9772
Simon Jefford <simon.jefford@gmail.com>
2013-08-30 10:05:38 +0100

    Make magit mode comfortable again.
    TODO - Warnings if top line more than 50...

--- personal/magit.el
+++ personal/magit.el
@@ -11,3 +11,22 @@
   (jump-to-register :magit-fullscreen))
 
 (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+
+(defun magit-commit-mode-init ()
+  (when (looking-at "\n")
+    (open-line 1)))
+
+(add-hook 'git-commit-mode-hook 'magit-commit-mode-init)
+
+;; close popup when commiting
+
+(defadvice git-commit-commit (after delete-window activate)
+  (delete-window))
+
+(defun magit-exit-commit-mode ()
+  (interactive)
+  (kill-buffer)
+  (delete-window))
+
+(eval-after-load "git-commit-mode"
+  '(define-key git-commit-mode-map (kbd "C-c C-k") 'magit-exit-commit-mode))
