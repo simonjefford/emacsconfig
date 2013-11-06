dd32d33f5d86e13a32e4fe9b16d7241be3e5a03c
Simon Jefford <simon.jefford@gmail.com>
2013-11-06 10:56:24 +0000

    deeper-blue = new default theme

--- core/prelude-ui.el
+++ core/prelude-ui.el
@@ -73,7 +73,7 @@
                                           "%b"))))
 
 ;; use zenburn as the default theme
-(load-theme 'zenburn t)
+(load-theme 'deeper-blue t)
 
 (provide 'prelude-ui)
 ;;; prelude-ui.el ends here
