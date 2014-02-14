8fef26e363fdb8f284462e2bafb31d543b8b035e
Simon Jefford <simon.jefford@gmail.com>
2014-02-14 09:52:56 +0000

    Revert "flycheck seems to be broken at the moment"
    This reverts commit 9ef518f4f91b986994db3c65fdbc89e0df003ae6.

--- modules/prelude-programming.el
+++ modules/prelude-programming.el
@@ -127,5 +127,10 @@ This functions should be added to the hooks of major modes for programming."
 (add-hook 'prog-mode-hook (lambda ()
                             (run-hooks 'prelude-prog-mode-hook)))
 
+;; enable on-the-fly syntax checking
+(if (fboundp 'global-flycheck-mode)
+    (global-flycheck-mode +1)
+  (add-hook 'prog-mode-hook 'flycheck-mode))
+
 (provide 'prelude-programming)
 ;;; prelude-programming.el ends here
