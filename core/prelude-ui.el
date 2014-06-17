6802ae141d2727f253f316743d07bf396b26e2a9
Simon Jefford <simon.jefford@gmail.com>
2014-06-17 23:48:23 +0100

    new default theme

--- core/prelude-ui.el
+++ core/prelude-ui.el
@@ -73,7 +73,7 @@
                                           "%b"))))
 
 ;; use zenburn as the default theme
-(load-theme 'zenburn t)
+(load-theme 'twilight-anti-bright t)
 
 (provide 'prelude-ui)
 ;;; prelude-ui.el ends here
