661fff96ffea1b9b12c0a24d2fdbaec1f5565f4c
Simon Jefford <simon.jefford@gmail.com>
2014-07-31 11:48:13 +0100

    Stop annoying company autocomplete behaviour

--- personal/_personalinit.el
+++ personal/_personalinit.el
@@ -92,7 +92,7 @@
 (require 'company)
 (require 'company-go)
 (setq company-tooltip-limit 20)                      ; bigger popup window
-(setq company-minimum-prefix-length 0)               ; autocomplete right after '.'
+(setq company-minimum-prefix-length 3)               ; autocomplete right after '.'
 (setq company-idle-delay .3)                         ; shorter delay before autocompletion popup
 (setq company-echo-delay 0)                          ; removes annoying blinking
 (setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
